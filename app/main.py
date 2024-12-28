2c604a628f81305bfe81eb616d275b5e63ea0508
Bahar Abdi <bahar.abdi04@gmail.com>
2024-12-27 22:49:47 -0600

    move sys.stdout into loop and add buffer flush

--- app/main.py
+++ app/main.py
@@ -3,15 +3,16 @@ import sys
 
 def main():
     #Uncomment this block to pass the first stage
-    sys.stdout.write("$ ")
     valid_command = []
 
     #Wait for user input
-    command = input()
     while True:
+        command = input()
+        sys.stdout.write("$ ")
+        sys.stdout.flush()
+
         if command not in valid_command:
             print(f"{command}: command not found")
-
             continue
 
 
