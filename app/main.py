90399fafd45e652bd445c4daa7bfd1e950c250ee
Bahar Abdi <bahar.abdi04@gmail.com>
2024-12-30 11:52:57 -0600

    include searching for files with PATH through type builtin

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 import sys
+import os
 
 
 def main():
@@ -18,9 +19,20 @@ def main():
             elif command.startswith("type "):
                 type = command[5:]
                 builtin = {"echo", "exit", "type"}
+
                 if type in builtin:
                     print(f"{type} is a shell builtin")
-                else:
+
+                path_dir = os.environ.get("PATH", "").split(":")
+                found = False
+                for directory in path_dir:
+                    potential_path = os.path.join(directory, type)
+                    if os.path.isfile(potential_path) and os.access(potential_path, os.X_OK):
+                        print(f"{type} is {potential_path}")
+                        found = True
+                        break
+
+                if not found:
                     print(f"{type}: not found")
 
             else:
