eb123ca552c2f2fe859f9ec41a6a2a456b756fe7
Bahar Abdi <bahar.abdi04@gmail.com>
2024-12-30 15:53:01 -0600

    add support for running external programs with args

--- app/main.py
+++ app/main.py
@@ -1,46 +1,60 @@
 import sys
 import os
+import subprocess
 
 
 def main():
-    try:
-        while True:
-            sys.stdout.write("$ ")
-            sys.stdout.flush()
+    while True:
+        sys.stdout.write("$ ")
+        sys.stdout.flush()
 
-            command = input()
+        command = input()
 
-            if command == "exit 0":
+        if command == "exit 0":
+            break
+
+        elif command.startswith("echo "):
+            print(command[5:])
+            continue
+
+        elif command.startswith("type "):
+            type = command[5:]
+            builtin = {"echo", "exit", "type"}
+
+            if type in builtin:
+                print(f"{type} is a shell builtin")
+                continue
+
+            path_dir = os.environ.get("PATH", "").split(":")
+            found = False
+            for directory in path_dir:
+                potential_path = os.path.join(directory, type)
+                if os.path.isfile(potential_path) and os.access(potential_path, os.X_OK):
+                    print(f"{type} is {potential_path}")
+                    found = True
+                    break
+
+            if not found:
+                print(f"{type}: not found")
+            continue
+
+        
+        cmd_args = command.split()
+        program = cmd_args[0]
+
+        path_dir = os.environ.get("PATH", "").split(":")
+        execute = None
+        for directory in path_dir:
+            potential_path = os.path.join(directory, program)
+            if os.path.isfile(potential_path) and os.access(potential_path, os.X_OK):
+                execute = potential_path
                 break
 
-            elif command.startswith("echo "):
-                print(command[5:])
+        if not execute:
+            print(f"{program}: command not found")
+            continue
 
-            elif command.startswith("type "):
-                type = command[5:]
-                builtin = {"echo", "exit", "type"}
-
-                if type in builtin:
-                    print(f"{type} is a shell builtin")
-                    continue
-
-                path_dir = os.environ.get("PATH", "").split(":")
-                found = False
-                for directory in path_dir:
-                    potential_path = os.path.join(directory, type)
-                    if os.path.isfile(potential_path) and os.access(potential_path, os.X_OK):
-                        print(f"{type} is {potential_path}")
-                        found = True
-                        break
-
-                if not found:
-                    print(f"{type}: not found")
-
-            else:
-                print(f"{command}: command not found")
-    except EOFError:
-        sys.exit(0)
-
+        subprocess.run([execute] + cmd_args[1:])
 
 if __name__ == "__main__":
     main()
