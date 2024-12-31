9371715610e68b7f4b87bf76fccda1e91b9243de
Bahar Abdi <bahar.abdi04@gmail.com>
2024-12-30 19:15:39 -0600

    include all support for redirecting output of command to file

--- app/main.py
+++ app/main.py
@@ -10,15 +10,33 @@ def main():
         sys.stdout.flush()
 
         command = input()
-        #test double quotes
+        
         cmd_args = shlex.split(command)
         program = cmd_args[0]
 
-        if command == "exit 0":
+        redirect_target = None
+        redirect_index = None
+
+        for i, arg in enumerate(cmd_args):
+            if arg in {">", "1>"}:
+                if i + 1 < len(cmd_args):
+                    redirect_target = cmd_args[i + 1]
+                    redirect_index = i
+                    break
+        
+        while redirect_target:
+            cmd_args = cmd_args[:redirect_index]
+
+        if program == "exit" and len(cmd_args) > 1 and cmd_args[1] == "0":
             break
 
         elif command.startswith("echo "):
-            print(" ".join(cmd_args[1:]))
+            output = " ".join(cmd_args[1:])
+            if redirect_target: #put into it's own func
+                with open(redirect_target, "w") as f:
+                    f.write(output)
+            else:
+                print(output.strip())
             continue
 
         elif command.startswith("type "):
@@ -27,23 +45,34 @@ def main():
 
             if type in builtin:
                 print(f"{type} is a shell builtin")
+            
+            else:
+                path_dir = os.environ.get("PATH", "").split(":")
+                found = False
+                for directory in path_dir:
+                    potential_path = os.path.join(directory, type)
+                    if os.path.isfile(potential_path) and os.access(potential_path, os.X_OK):
+                        print(f"{type} is {potential_path}")
+                        found = True
+                        break
+
+                if not found:
+                    print(f"{type}: not found")
+
+                if redirect_target:
+                    with open(redirect_target, "w") as f:
+                        f.write(output)
+                else:
+                    print(output.strip())
                 continue
 
-            path_dir = os.environ.get("PATH", "").split(":")
-            found = False
-            for directory in path_dir:
-                potential_path = os.path.join(directory, type)
-                if os.path.isfile(potential_path) and os.access(potential_path, os.X_OK):
-                    print(f"{type} is {potential_path}")
-                    found = True
-                    break
-
-            if not found:
-                print(f"{type}: not found")
-            continue
-
         elif command == "pwd":
-            print(os.getcwd())
+            output = os.getcwd()
+            if redirect_target:
+                with open(redirect_target, "w") as f:
+                    f.write(output)
+            else:
+                print(output.strip())
             continue
 
         elif command.startswith("cd"):
@@ -69,7 +98,8 @@ def main():
             print(f"{program}: command not found")
             continue
 
-        subprocess.run([execute] + cmd_args[1:])
+        with open(redirect_target, "w") if redirect_target else sys.stdout as f:
+            subprocess.run([execute] + cmd_args[1:], stdout=f, stderr=sys.stderr)
 
 if __name__ == "__main__":
     main()
